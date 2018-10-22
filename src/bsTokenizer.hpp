90ff6a2314925df7cd94f2454f6536cffff8890f
EdwinRy <edwinrybarczyk4d@gmail.com>
2018-10-22 18:00:03 +0100

    k

--- src/bsTokenizer.hpp
+++ src/bsTokenizer.hpp
@@ -7,8 +7,8 @@ namespace BuildScript
     {
         Unrecognised,
         Identifier,
-        Number,
         String,
+        Number,
         OpenParam,
         CloseParam,
         OpenScope,
